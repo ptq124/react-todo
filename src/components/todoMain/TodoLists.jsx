facf257708ad057800046ad1fa7da5976528a35e
박태규 Park Tae Kyu <ptq124@naver.com>
2023-01-17 02:25:16 +0900

    feat: 필터기능 추가

--- src/components/todoMain/TodoLists.jsx
+++ src/components/todoMain/TodoLists.jsx
@@ -1,7 +1,7 @@
 import {useState} from 'react'
 import Todo from './Todo'
 import TodoButton from './TodoButton'
-export default function TodoLists(){
+export default function TodoLists({filter}){
   const [todoList, setTodoList] = useState([
     {
       id: 1,
@@ -22,10 +22,28 @@ export default function TodoLists(){
   const addTodoList = (value) =>{
     setTodoList([...todoList, value])
   }
+  const deleteTodoList = (id) =>{
+    setTodoList(todoList.filter(d=> d.id !== id))
+  }
+  const changeStatus = (todo) => {
+    setTodoList(todoList.map(d => d.id === todo.id ? todo : d ))
+  }
+  const filtered = getfilter(todoList, filter)
   return (
     <section>
-      <Todo todoList={todoList}></Todo>
+      <ul>
+        {filtered.map((d, index)=>(
+          <Todo key={index} todo={d} deleteTodoList={deleteTodoList} changeStatus={changeStatus}></Todo>
+        ))}
+      </ul> 
       <TodoButton addTodoList={addTodoList}></TodoButton>
     </section>
   )
+}
+
+function getfilter(todoList, status){
+  if(status === 'all'){
+    return todoList
+  }
+  return todoList.filter(d => d.status === status)
 }
