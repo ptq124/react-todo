7d77cbb00a67ffba0f194d410366d746054ab877
박태규 Park Tae Kyu <ptq124@naver.com>
2023-01-17 02:23:03 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-//import TodoHeader from './components/todoHead/TodoHeader'
+import { useState } from 'react';
+import TodoHeader from './components/todoHead/TodoHeader'
 import TodoLists from './components/todoMain/TodoLists'
+
+const filters = ['all', 'active', 'complete']
 export default function App() {
+  const [filter, setfilter] = useState(filters[0]) 
+  const handlefilter = (option) => {
+    setfilter(option)
+  }
   return (
     <>
-      {/* <TodoHeader></TodoHeader> */}
-      <TodoLists></TodoLists>
+      <TodoHeader filters={filters} handlefilter={handlefilter}></TodoHeader>
+      <TodoLists filter={filter}></TodoLists>
     </>
   );
 }
